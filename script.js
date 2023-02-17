5aa863dd8c4782860e7d35bc8b99f90dbe247721
Rhys Mills <rhystmills@gmail.com>
2023-02-17 17:00:36 +0000

    More game stuff

--- script.js
+++ script.js
@@ -6,41 +6,52 @@
 const rolls = {
     sideDot: {
         probability: 349,
+        cumulative: 349,
         points: 5,
         name: "Side (dot)",
         id: "sideDot"
     },
     sideBlank: {
         probability: 302,
+        cumulative: 651,
         points: 5,
         name: "Side (no dot)",
         id: "sideBlank"
     },
     razorback: {
         probability: 224,
+        cumulative: 875,
         points: 5,
         name: "Razorback",
         id: "razorback"
     },
     trotter: {
         probability: 88,
+        cumulative: 963,
         points: 5,
         name: "Trotter",
         id: "trotter"
     },
     snouter: {
         probability: 30,
+        cumulative: 993,
         points: 5,
         name: "Snouter",
         id: "snouter"
     },
     jowler: {
-        probability: 6,
+        probability: 7,
         points: 5,
+        cumulative: 1000,
         name: "Leaning Jowler",
         id: "jowler"
     },
 }
+
+const totalProbabilities = Object.values(rolls).reduce((acc, cur) => acc + cur.probability, 0)
+
+const getRolledPosition = (number) =>  Object.values(rolls).sort((a,b) => a.cumulative - b.cumulative).find(roll => number <= roll.cumulative).id;
+console.log(getRolledPosition);
 
 const config = {
     maxPlayers: 100
@@ -65,8 +76,8 @@ class Game {
 
 class Player {
     #active = false;
-    #tempScore = 0;
-    #permScore = 0;
+    roundScore = 0;
+    #totalScore = 0;
     constructor(name, id) {
         this.name = name
         this.id = id
@@ -77,31 +88,41 @@ class Player {
     getActive = () => {
         return this.#active;
     }
+    getTotalScore = () => {
+        return this.#totalScore;
+    }
     nextPlayer = () => {
         this.setActive(false);
+        const nextPlayerId = getNextPlayer(this.id);
+        gameData.currentPlayer = nextPlayerId;
         //TODO: mechanism for going to next player?
     }
-    addToTempScore = (num) => {
-        this.#tempScore += num;
+    addToRoundScore = (num) => {
+        this.roundScore += num;
         // TODO: if tempScore + permScore >= 100 - you win
     }
+    
     stick = () => {
-        this.#permScore += this.#tempScore;
+        this.#totalScore += this.roundScore;
         this.nextPlayer();
     }
     roll = () => {
+        const roll = Math.ceil(Math.random() * 1000);
+        const positionId = getRolledPosition(roll)
+        console.log({roll, positionId});
+
         //TODO: do two rolls
         // Work out combined score
         // Add to tempscore Or end go
         // If pigOut - go to next player
     }
     pigOut = () => {
-        this.#tempScore = 0;
+        this.roundScore = 0;
         this.nextPlayer();
     }
     oinker = () => {
-        this.#tempScore = 0;
-        this.#permScore = 0;
+        this.roundScore = 0;
+        this.#totalScore = 0;
         this.nextPlayer();
     }
 }
@@ -187,6 +208,10 @@ const submitPlayers = (event) => {
     // move to next screen if it's valid
 }
 
+const nextPlayerIsPlayerZero = (playerId) => playerId < gameData.playerTotal - 1 ? false : true;
+
+const getNextPlayer = (playerId) => nextPlayerIsPlayerZero(playerId) ? 0 : playerId + 1;
+
 const submitPlayerNumber = (event) => {
     event.preventDefault();
     const desiredName = event.target[0].value;
@@ -194,7 +219,7 @@ const submitPlayerNumber = (event) => {
         gameData.players.push(new Player(desiredName))
         const playerid = event.target.dataset.playerid;
         console.log({event})
-        if (playerid < gameData.playerTotal-1){
+        if (!nextPlayerIsPlayerZero(playerid)){
             // There are players left to name
             advanceScreen(`playerNameElement${playerid}`, `playerNameElement${Number(playerid)+1}`);
         } else {
@@ -221,6 +246,7 @@ const createNameElement = (number, active) => {
 }
 
 const createNameElements = (numberOfPlayers) => {
+    // TODO: focus the input
     const nameScreen = getEl("nameScreen");
     for (let i = 0; i < numberOfPlayers; i++){
         const playerNameElement = createNameElement(i, i===0 ? true : false);
@@ -240,14 +266,27 @@ const getPigImage = (pigName, position) => {
 const createGameFrame = (playerId) => {
     const gameFrame = getEl("gameFrame");
     const pigBlanket = getEl("pigBlanket");
+    const totalScoreEl=getEl("totalScore");
+    const roundScoreEl=getEl("roundScore");
+    const currentPlayerEl=getEl("currentPlayer");
+    const rollButton=getEl("rollButton");
+
     const pigLeft = getPigImage(gameData.pigs.left);
     const pigRight = getPigImage(gameData.pigs.right);
-    console.log({pigBlanket})
-    wipeElement(pigBlanket);
-    console.log({pigBlanket})
+
+    const player = gameData.players[playerId];
+    const totalScore = player.getTotalScore();
+    console.log({player, playerId, totalScore})
+    totalScoreEl.append(createTextNode(totalScore));
+    roundScoreEl.append(createTextNode(0));
+    currentPlayerEl.append(createTextNode(player.name));
 
     pigBlanket.appendChild(pigLeft);
     pigBlanket.appendChild(pigRight);
+
+    rollButton.addEventListener("mousedown",(e) => {
+        player.roll()
+    })
 }
 
 
